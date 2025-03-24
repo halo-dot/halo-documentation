66ebf5695ba1cb5db1cae8f71dd02ee97dc004d4
MuhiyaSumba <muhiya@synthesis.co.za>
2025-03-24 08:39:51 +0200

    HHFE-266 Update algolia config and robots.txt

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,7 +63,6 @@ const config: Config = {
   ],
 
   plugins: [],
-  // themes: ['@docusaurus/theme-search-algolia'],
   themeConfig: {
     image: 'https://cdn.prod.website-files.com/63f8ad30f40a41f7b046d567/67ac8d81adc0bbd4b1e9e0dc_Image.png',
     navbar: {
@@ -145,38 +144,38 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
-    // algolia: {
-    //   // The application ID provided by Algolia
-    //   appId: 'YOUR_APP_ID_dummy',
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'S2W3MQE857',
 
-    //   // Public API key: it is safe to commit it
-    //   apiKey: 'YOUR_SEARCH_API_KEY_dummy',
+      // Public API key: it is safe to commit it
+      apiKey: '30114c9f8022e361f11991f495f93d1a',
 
-    //   indexName: 'YOUR_INDEX_NAME',
+      indexName: 'halo',
 
-    //   // // Optional: see doc section below
-    //   // contextualSearch: true,
+      // // Optional: see doc section below
+      // contextualSearch: true,
 
-    //   // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-    //   // externalUrlRegex: 'external\\.com|domain\\.com',
+      // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
 
-    //   // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-    //   // replaceSearchResultPathname: {
-    //   //   from: '/docs/', // or as RegExp: /\/docs\//
-    //   //   to: '/',
-    //   // },
+      // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
 
-    //   // Optional: Algolia search parameters
-    //   searchParameters: {},
+      // Optional: Algolia search parameters
+      searchParameters: {},
 
-    //   // Optional: path for search page that enabled by default (`false` to disable it)
-    //   searchPagePath: 'search',
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
 
-    //   // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-    //   insights: false,
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
 
-    //   //... other Algolia params
-    // },
+      //... other Algolia params
+    },
 
   } satisfies Preset.ThemeConfig,
 };
