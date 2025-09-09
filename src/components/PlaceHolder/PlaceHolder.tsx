aa68e1f88a6d9b7f5b66605e3948d449c31cfda9
frederick <frederick@synthesis.co.za>
2025-09-09 12:07:41 +0200

    HHFE-396: Explanation of new fields in the callback webhook

--- src/components/PlaceHolder/PlaceHolder.tsx
+++ src/components/PlaceHolder/PlaceHolder.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+
+/**
+ * @example
+ * <PlaceHolder>Default content or children</PlaceHolder>
+ * @returns
+ */
 export default function PlaceHolder({children}: {children: React.ReactNode}) {
   return (
       <span className="placeholder">
